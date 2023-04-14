4fec69000b43dd95d6f6d470b6de16c445c773e1
unknown <thc101001@gmail.com>
2023-04-15 00:07:27 +0700

    first

--- controller/ListController.py
+++ controller/ListController.py
@@ -5,18 +5,21 @@ from model import ToDoList
 
 class ListController(Resource):
 
+    # params: {userId:String}
     def get(self,userId):
         return ToDoList.getList(userId)
-    
+
+    # args: {userId:String, date:String, start:String, task:String}  
     def post(self):
         userId = request.form['userId']
         date = request.form['date']
         start = request.form['start']
         task = request.form['task']
 
-        ToDoList.createList(userId, date, start, task)
+        ToDoList.createList(userId, date, start, task) 
         return jsonify(ToDoList.createList(userId, date, start, task))
 
+    # args: {userId:String, date:String, start:String, task:String}
     def delete(self):
         userId = request.form['userId']
         date = request.form['date']
