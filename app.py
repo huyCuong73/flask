be651a8cb4aba79a3d0add12380882aa70b41574
Tạ Quang Toàn <toanta0326@gmail.com>
2023-04-15 11:16:54 +0700

    change url mongo

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from routers import router
 
 
 app = Flask(__name__)
-
 router.router(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
 
--- routers/router.py
+++ routers/router.py
@@ -3,7 +3,6 @@ from controller import ListController
 
 def router(app):
     route = Api(app)
-
     route.add_resource(ListController.ListController, '/delete-task', endpoint="delete-task")
     route.add_resource(ListController.ListController, '/', endpoint="list")
     route.add_resource(ListController.ListController, '/get-list/<string:userId>', endpoint="get-list")
